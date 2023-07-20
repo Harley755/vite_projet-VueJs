c2a487327c3af0bf805c5d21d1b2cbeb1b12b597
Brice G <bricegoudalo@gmail.com>
2023-07-20 12:59:01 +0200

    use composables

--- src/views/ContactView.vue
+++ src/views/ContactView.vue
@@ -1,6 +1,32 @@
+<script setup>
+import { ref, watch } from 'vue'
+import { useStorage } from '@/composables/useStorage.js'
+
+let food = useStorage('food')
+
+// let food = ref(localStorage.getItem('food'))
+// let age = ref(localStorage.getItem('age'))
+
+// watch(food, (val) => {
+//   write('food', val)
+// })
+
+// watch(age, (val) => {
+//   write('age', val)
+// })
+
+// setTimeout(() => {
+//   food.value = 'changed !'
+// }, 2000)
+</script>
+
+
 <template>
   <div class="about">
-    <h1>This is an contact page</h1>
+    <p>What is ur favorite food ?</p>
+    <input type="text" v-model="food" />
+    <p>How old are u</p>
+    <input type="number" v-model="age" />
   </div>
 </template>
 
