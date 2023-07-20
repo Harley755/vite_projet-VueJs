cde6cbbb2569736a478d0d10fc0fb43b522172c2
Brice G <bricegoudalo@gmail.com>
2023-07-20 10:44:14 +0200

    work with script setup and without setup

---
+++ src/views/HomeView.vue
@@ -0,0 +1,24 @@
+<script>
+import { ref } from 'vue'
+import TheWelcome from '../components/TheWelcome.vue'
+
+export default {
+  components: { TheWelcome },
+
+  setup() {
+    let message = ref('Hello World')
+    return {
+      message
+    }
+  }
+}
+</script>
+
+<template>
+  <main>
+    <TheWelcome />
+    {{ message }}
+
+    <input type="text" v-model="message" />
+  </main>
+</template>
